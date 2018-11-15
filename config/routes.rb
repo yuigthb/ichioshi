0689f488a657cf71cc0d05f5c4a710cb57c1983b
EC2 Default User <ec2-user@ip-172-31-11-228.us-east-2.compute.internal>
2018-11-15 05:55:06 +0000

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  get '/about', to:'static_pages#about'
 
-  get 'static_pages/setting'
+  get '/setting', to:'static_pages#setting'
 
-  get 'static_pages/help'
+  get '/help', to:'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
